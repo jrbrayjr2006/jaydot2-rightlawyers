0078266ca704d54ca52070b11a97f3a14eabebe0
James Bray <jrbrayjr2006@gmail.com>
2014-09-04 19:59:24 -0500

    Added new fragment classes and supporting properties

---
+++ RightLawyersMobile/src/com/rightlawers/rightlawyersmobile/LegalFormFragment.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.rightlawers.rightlawyersmobile;
+
+import android.app.Fragment;
+
+/**
+ * @author james_r_bray
+ *
+ */
+public class LegalFormFragment extends Fragment {
+
+}
---
+++ RightLawyersMobile/src/com/rightlawyers/rightlawyersmobile/model/TrafficTicket.java
@@ -0,0 +1,84 @@
+package com.rightlawyers.rightlawyersmobile.model;
+
+import java.util.Date;
+
+public class TrafficTicket {
+	
+	private String firstName;
+	private String lastName;
+	private String cellPhone;
+	private String email;
+	private String citationNumber;
+	private String municipality;
+	private Date courtDate;
+	private Byte[] photo;
+	
+	public TrafficTicket() {
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCitationNumber() {
+		return citationNumber;
+	}
+
+	public void setCitationNumber(String citationNumber) {
+		this.citationNumber = citationNumber;
+	}
+
+	public String getMunicipality() {
+		return municipality;
+	}
+
+	public void setMunicipality(String municipality) {
+		this.municipality = municipality;
+	}
+
+	public Date getCourtDate() {
+		return courtDate;
+	}
+
+	public void setCourtDate(Date courtDate) {
+		this.courtDate = courtDate;
+	}
+
+	public Byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(Byte[] photo) {
+		this.photo = photo;
+	}
+	
+}
