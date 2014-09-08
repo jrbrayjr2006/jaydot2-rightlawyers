93b94e2a36f49ea91bd7272375f3fe393e9c3f40
James Bray <jrbrayjr2006@gmail.com>
2014-09-08 18:54:36 -0500

    Added email helper class for email functionality

---
+++ RightLawyersMobile/src/com/rightlawyers/rightlawyersmobile/helper/EmailHelper.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.rightlawyers.rightlawyersmobile.helper;
+
+/**
+ * @author james_r_bray
+ *
+ */
+public class EmailHelper {
+	
+	public EmailHelper() {}
+	
+	/**
+	 * Constructs the body of the email message
+	 * @return
+	 */
+	public String buildEmailBody() {
+		StringBuffer body = new StringBuffer();
+		
+		//TODO add logic to support building email body
+		
+		return body.toString();
+	}
+
+}
