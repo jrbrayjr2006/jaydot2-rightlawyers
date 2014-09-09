b31bffbcdfc1584f988322eb38df28f3ced6c8c3
James Bray <jrbrayjr2006@gmail.com>
2014-09-08 21:16:47 -0500

    Add AccidentTO (transfer object) and modify email helper

--- RightLawyersMobile/src/com/rightlawyers/rightlawyersmobile/helper/EmailHelper.java
+++ RightLawyersMobile/src/com/rightlawyers/rightlawyersmobile/helper/EmailHelper.java
@@ -3,6 +3,8 @@
  */
 package com.rightlawyers.rightlawyersmobile.helper;
 
+import com.rightlawyers.rightlawyersmobile.to.AccidentTO;
+
 /**
  * @author james_r_bray
  *
@@ -15,11 +17,30 @@ public class EmailHelper {
 	 * Constructs the body of the email message
 	 * @return
 	 */
-	public String buildEmailBody() {
+	public String buildEmailBody(AccidentTO ato) {
 		StringBuffer body = new StringBuffer();
 		
 		//TODO add logic to support building email body
-		
+		body.append("Accident Report\n\n");
+    	body.append("Your Name: " + ato.yourName);
+    	body.append("\nYour Email: " + ato.yourEmail);
+    	body.append("\nYour Phone: " + ato.yourPhone);
+    	body.append("\nOther Name: " + ato.otherName);
+    	body.append("\nOther Email:" + ato.otherEmail);
+    	body.append("\nOther Phone:" + ato.otherPhone);
+    	body.append("\n\nCar Info\n\n");
+    	body.append("Your Make: " + ato.yourMake);
+    	body.append("\nYour Model: " + ato.yourModel);
+    	body.append("\nYour License/Year: " + ato.yourLicense);
+    	body.append("\nOther Make: " + ato.otherMake);
+    	body.append("\nOther Model: " + ato.otherModel);
+    	body.append("\nOther License/Year: " + ato.otherLicense);
+    	body.append("\n\nPolice Info:\n\n");
+    	body.append("Officer Name: " + ato.officerName);
+    	body.append("\nReport Number: " + ato.reportNumber);
+    	body.append("\nCity/County: " + ato.cityCounty);
+    	body.append("\n\nNotes\n\n" + ato.notes);
+    	
 		return body.toString();
 	}
 
---
+++ RightLawyersMobile/src/com/rightlawyers/rightlawyersmobile/to/AccidentTO.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.rightlawyers.rightlawyersmobile.to;
+
+/**
+ * @author james_r_bray
+ *
+ */
+public class AccidentTO {
+	
+	public String yourName;
+	public String yourEmail;
+	public String yourPhone;
+	public String otherName;
+	public String otherPhone;
+	public String otherEmail;
+	public String yourMake;
+	public String yourModel;
+	public String yourLicense;
+	public String otherMake;
+	public String otherModel;
+	public String otherLicense;
+	public String officerName;
+	public String reportNumber;
+	public String cityCounty;
+	public String notes;
+
+	/**
+	 * 
+	 */
+	public AccidentTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
