ab480abbac6ba13888d7b51306f8d75d468355ef
James Bray <jrbrayjr2006@gmail.com>
2014-09-25 20:14:40 -0500

    refactored the package

---
+++ RightLawyersMobile/src/com/rightlawyers/rightlawyersmobile/SplashScreenFragment.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.rightlawyers.rightlawyersmobile;
+
+import com.rightlawers.rightlawyersmobile.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author james_r_bray
+ *
+ */
+public class SplashScreenFragment extends Fragment {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+		super.onCreateView(inflater, parent, savedInstanceState);
+		View v = inflater.inflate(R.layout.fragment_main, parent, false);
+		
+		return v;
+	}
+
+}
